c4e967d35a1621ca3e6a268b87c0484cb752ae52
clement-machtelinckx <clement.machtelinckx@laplateforme.io>
2026-01-19 11:15:55 +0100

    first commit

---
+++ src/tp9/Book.java
@@ -0,0 +1,21 @@
+package tp9;
+
+// Classe Book
+public class Book implements IBook {
+    private String title;
+    private double price;
+
+    public Book (String title, double price)
+    {
+        //TODO à compléter
+    }
+    // getters, setters, etc.
+
+
+    //TODO à compléter
+
+    public String preview() {
+        // Simule l'affichage d'un aperçu du livre (indiquez par exemple "Preview of Clean Code: [First few lines of the book...]")
+        return null;
+    }
+}
---
+++ src/tp9/adapter/BookStoreAdapter.java
@@ -0,0 +1,18 @@
+package tp9.adapter;
+
+import tp9.Book;
+
+public class BookStoreAdapter implements IBookStore {
+    // TODO trouver quels sont le ou les attributs nécessaires
+    private OldBookStore oldBookStore;
+
+    public BookStoreAdapter(OldBookStore oldBookStore) {
+        // TODO écrivez ce constructeur
+    }
+
+    @Override
+    public Book fetchBook(String title) {
+        // TODO à vous de faire en sorte que fetchBook exploite l'ancien store pour récupérer l'info du livre en question
+        return null ;
+    }
+}
